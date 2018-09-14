fbc4eefacca453564a4555271f8dac73d131fae9
Alexandra Fabricius Porsgaard <alexandraporsgaard@au.dk>
2018-09-14 17:34:21 +0200

    changed ledsPlugin to initialize all three leds

--- plugins/internal/ledsPlugin.js
+++ plugins/internal/ledsPlugin.js
@@ -1,7 +1,11 @@
 var resources = require('./../../resources/model');
 
 var actuator, interval;
-var model = resources.pi.actuators.leds['1'];
+
+var model1 = resources.pi.actuators.leds['1'],
+    model2 = resources.pi.actuators.leds['2'],
+    model3 = resources.pi.actuators.leds['3'];
+
 var pluginName = model.name;
 var localParams = {'simulate': false, 'frequency': 2000};
 
@@ -10,7 +14,7 @@ exports.start = function (params) {
    resources.observe(changes => {
     changes.forEach(change => {
       if (change.type === 'update' &&
-          model === change.path.slice(0, -1).reduce((obj, i) => obj[i], resources)) {
+          model1 | model2 | model3 === change.path.slice(0, -1).reduce((obj, i) => obj[i], resources)) {
         switchOnOff(change.value);
       }
     });
@@ -42,17 +46,25 @@ function switchOnOff(value) {
 
 function connectHardware() {
   var Gpio = require('onoff').Gpio;
-  actuator = new Gpio(model.gpio, 'out'); //#D
+  actuator1 = new Gpio(model1.gpio, 'out');
+  actuator2 = new Gpio(model2.gpio, 'out');
+  actuator3 = new Gpio(model3.gpio, 'out');
   console.info('Hardware %s actuator started!', pluginName);
 };
 
 function simulate() {
   interval = setInterval(function () {
     // Switch value on a regular basis
-    if (model.value) {
-      model.value = false;
+    if (model1.value) {
+      model1.value = false;
+    } else if (model2.value) {
+      model2.value = false;
+    } else if (model3.value){
+      model3.value = false;
     } else {
-      model.value = true;
+      model1.value = true;
+      model2.value = true;
+      model3.value = true;
     }
   }, localParams.frequency);
   console.info('Simulated %s actuator started!', pluginName);
