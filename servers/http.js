601c3e644a62d2852db729abcdfc154be4cc39a8
Alexandra <you@example.com>
2018-09-20 20:07:17 +0200

    Action for blue led works

--- servers/http.js
+++ servers/http.js
@@ -13,7 +13,8 @@ var thngId=config.thngId;
 var thngUrl='/thngs/'+thngId;
 var thngApiKey=config.thngApiKey; 
 
-var status=false;
+//var status=false;
+var led;
 var updateInterval;
 
 var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", {
@@ -30,25 +31,33 @@ client.on('connect', function () {
 
 client.on('message', function(topic, message) {
   var resources = topic.split('/');
-  if (resources[1] && resources[1] === "thngs"){ // #B
-    if (resources[2] && resources[2] === thngId){  // #C
-      if (resources[3] && resources[3] === "properties"){ //#D
+  if (resources[1] && resources[1] === "thngs"){
+    if (resources[2] && resources[2] === thngId){
+      if (resources[3] && resources[3] === "properties"){
         var property = JSON.parse(message);
-        console.log('Property was updated: '+property[0].key+'='+property[0].value); 
-      } else if (resources[3] && resources[3] === "actions"){ //#E
+        console.log('Property was updated: '+property[0].key+'='+property[0].value);
+      } else if (resources[3] && resources[3] === "actions"){
         var action = JSON.parse(message);
-        handleAction(action); 
+        handleAction(action);
       }
     }
   }
 });
 
 function handleAction(action) {
-  switch(action.type) { // #F
-    case '_setStatus':
-      console.log('ACTION: _setStatus changed to: '+action.customFields.status); // #G
-      status=Boolean(action.customFields.status);
-      updateProperty ('status',status);
+  switch(action.type) {
+    case '_setLedState':
+      console.log('ACTION: _setLedState changed the '+action.customFields.led +' led');
+      led = action.customFields.led;
+      if (led === "blue"){
+        var blueLed = resources.pi.actuators.leds['1'];
+	if (blueLed.value) {
+	   blueLed.value = false;
+        } else {
+	   blueLed.value = true;
+	}
+      }
+      else console.log("Det var ikke den blaa led");
       /* Do something else too */
       break;
     case '_setLevel':
